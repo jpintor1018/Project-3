7f39b0ad723b7857233cf9c51ee760cabf00d5b1
Orhys Brown <51386750+orhys@users.noreply.github.com>
2019-11-16 16:09:39 -0500

    remove passport.js

--- client/src/passport.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppContainer from "./Components/Auth/AppContainer";
-import "./index.css";
-
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
