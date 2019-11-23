c3aae85643881998583669131269329d13ccd807
John Michael Pintor <jpintor1018@yahoo.com>
2019-11-23 12:41:42 -0500

    Updated Home Page

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,30 +1,39 @@
 import React from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "../Navbar";
-import Seating from "../Seating";
-import Date from "../Date";
+// import Seating from "../Seating";
+// import Date from "../Date";
 import Parallax from "../Parallax";
-// import Host from "./pages/Host"
-// import Discover from "./pages/Discover";
-// import About from "./pages/About";
-// import Search from "./pages/Search";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "../Button";
 import "./style.css";
 
 const Home = () => (
-    <div className="main-body">
-        <Navbar />
-
-    <div className="parallax"> <Parallax/>  </div>
+ 
+  <div className="main-body">
+    <Navbar />
+    <div className="parallax">
+      <Parallax />
     </div>
-    // <Router>
-    //   <div>
-    //     <Navbar />
-    //     <Switch>
-    //       <Route exact path="/" component={Host} />
-    //     </Switch>
-    //   </div>
-    // </Router>
-  );
+    <div className="row">
+      <div className="col">
+        <div className="container">
+          <h1 className="animated rotateIn ">Make your Reservations</h1>
+          <div id="contain-btn" className="animated rollIn">
+            <div id="button" style={{ position: "relative", zIndex: "1" }}></div>
+            <App />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  // <Router>
+  //   <div>
+  //     <Navbar />
+  //     <Switch>
+  //       <Route exact path="/" component={Host} />
+  //     </Switch>
+  //   </div>
+  // </Router>
+);
 
 export default Home;
